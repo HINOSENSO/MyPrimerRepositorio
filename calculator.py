c9a27794a39ed5a2d718a56a3526791d8e71fd73
Juan <hinosenso@gmail.com>
2024-03-10 00:07:32 +0100

    modificacion calculadora

---
+++ calculator.py
@@ -0,0 +1,19 @@
+print("CALCULADORA")
+
+eleccion = 0
+A = 0
+B = 0
+
+while eleccion != 6:
+    print("""
+    Indique la operación a realizar:
+    1) Suma
+    2) Resta
+    3) Multiplicación
+    4) División
+    5) Cambio de valores
+    6) Salir
+    """)
+
+    eleccion = int(input("Ingrese el número de la operación deseada: "))
+
